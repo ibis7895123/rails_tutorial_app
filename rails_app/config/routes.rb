209923eecc4cf5a362d7a09eacc07e1adbddd402
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-07 01:06:17 +0900

    Userページ、モデルをスキャフォールドした

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -3,6 +3,6 @@ Rails
   .routes
   .draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root 'application#hello'
-    root 'application#goodbye'
+    root 'users#index'
+    resources :users
   end
