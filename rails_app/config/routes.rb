e35936b2f863dbc4c22539f592ddef80010e8973
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-08 17:57:09 +0900

    aboutページを作成

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -5,6 +5,7 @@ Rails
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'static_pages/home'
     get 'static_pages/help'
+    get 'static_pages/about'
     root 'users#index'
     resources :users
     resources :microposts
