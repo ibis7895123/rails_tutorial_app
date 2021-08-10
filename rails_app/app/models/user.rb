9fe40ec18f09c605d95e4c24c026de9c317d756b
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-10 15:18:47 +0900

    メールアドレスの有効性バリデーション

--- rails_app/app/models/user.rb
+++ rails_app/app/models/user.rb
@@ -1,4 +1,14 @@
+# aaa@sample.com のようなメールアドレスかどうかをチェックする
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email,
+            presence: true,
+            length: {
+              maximum: 255
+            },
+            format: {
+              with: VALID_EMAIL_REGEX
+            }
 end
