e4113299e305e804c142e974336728c913c943e2
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-10 15:53:34 +0900

    メールアドレスは大文字小文字を区別しないように

--- rails_app/app/models/user.rb
+++ rails_app/app/models/user.rb
@@ -2,6 +2,8 @@
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 class User < ApplicationRecord
+  # モデル保存前にメールアドレスを小文字に変換する
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email,
             presence: true,
