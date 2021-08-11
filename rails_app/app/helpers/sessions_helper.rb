16ddcf839498e7ddff0b9e491e632c066b9139ec
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-11 22:08:30 +0900

    ログインユーザーの取得ロジックを作成

--- rails_app/app/helpers/sessions_helper.rb
+++ rails_app/app/helpers/sessions_helper.rb
@@ -3,4 +3,19 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # ログインしているユーザーを返す
+  def current_user
+    # 未ログインならnilを返す
+    if !session[:user_id]
+      puts 'not found user id.'
+      return nil
+    end
+
+    # idからユーザーを取得して返す
+    # 2回目以降はキャッシュを返す
+    return @current_user if !@current_user.nil?
+
+    return @current_user = User.find_by(id: session[:user_id])
+  end
 end
