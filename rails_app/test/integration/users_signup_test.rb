6ad8175fe26cac47ea18bb4d3c13c23722c032b3
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-11 12:12:55 +0900

    ユーザー新規登録のテストを作成

---
+++ rails_app/test/integration/users_signup_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+
+    # post前後で「User.count」の値が変化していないことをチェック
+    assert_no_difference 'User.count' do
+      post users_path,
+           params: {
+             user: {
+               name: '',
+               email: 'user@invalid',
+               password: 'foo',
+               password_confirmation: 'bar'
+             }
+           }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+end
