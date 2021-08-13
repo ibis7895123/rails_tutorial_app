18f6395ff8f87f86999d1984f3c926aa380ad5d6
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-13 14:11:57 +0900

    ログインのrememberトークンロジックを作成

--- rails_app/app/models/user.rb
+++ rails_app/app/models/user.rb
@@ -2,6 +2,8 @@
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   # モデル保存前にメールアドレスを小文字に変換する
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -19,6 +21,13 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  def remember
+    @remember_token = User.new_token
+
+    # バリデーションをスルーしてレコードを更新
+    update_attribute(:remember_digest, User.digest(@remember_token))
+  end
+
   # 渡された文字列のハッシュ値を返す
   def self.digest(string)
     # 暗号化コスト設定
@@ -30,5 +39,10 @@ class User < ApplicationRecord
       end
 
     return BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す(22文字)
+  def self.new_token
+    return SecureRandom.urlsafe_base64
   end
 end
