0e3e657e807b0a109f57d74556548258c6c9bad0
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-16 18:10:30 +0900

    fakerを使ってseedできるようにした

--- rails_app/db/seeds.rb
+++ rails_app/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: 'Example User',
+  email: 'example@railstutorial.org',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+99.times do |time|
+  name = Faker::Name.name
+  email = "example-#{time + 1}@railstutorial.org"
+  password = 'password'
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
