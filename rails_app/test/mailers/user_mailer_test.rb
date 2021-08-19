3ac89d9efdcdf7cf74ea580c2ed038a08be4436b
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-19 12:19:11 +0900

    メールのテスト追加

--- rails_app/test/mailers/user_mailer_test.rb
+++ rails_app/test/mailers/user_mailer_test.rb
@@ -18,11 +18,18 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email), mail.body.encoded
   end
 
-  test 'password_reset' do
-    mail = UserMailer.password_reset
+  test '正常系_パスワードリセット' do
+    # リセットトークンを持つユーザーを定義
+    user = users(:michael)
+    user.reset_token = User.new_token
+
+    # メールを作成
+    mail = UserMailer.password_reset(user)
+
+    # タイトル、送信先などをチェック
     assert_equal 'Password reset', mail.subject
-    assert_equal ['to@example.org'], mail.to
-    assert_equal ['noreply@example.com'], mail.from
-    assert_match 'Hi', mail.body.encoded
+    assert_equal [user.email], mail.to
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
   end
 end
