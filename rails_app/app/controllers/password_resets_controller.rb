c81ca610ed4931a404cd2545c1af6868e255a9f0
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-19 16:32:10 +0900

    新パスワード入力フォームの作成

--- rails_app/app/controllers/password_resets_controller.rb
+++ rails_app/app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+
   def new; end
 
   def create
@@ -23,4 +26,22 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit; end
+
+  # メールアドレスからユーザーを取得
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+  private :get_user
+
+  # 正しいユーザーかどうか確認する
+  def valid_user
+    # 1．ユーザーが存在する
+    # 2．ユーザーが有効化済
+    # 3．リセットトークンが正しい
+    # 場合**以外**はHOMEに戻す
+    unless @user && @user.activated? &&
+             @user.authenticated?(:reset, params[:id])
+      redirect_to root_path
+    end
+  end
 end
