1a6f96b3a41769bba25c2f5da8912202ab4de58e
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-23 13:11:20 +0900

    ユーザー詳細のテストを追加

--- rails_app/app/helpers/application_helper.rb
+++ rails_app/app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def full_title(page_title = '')
     base_title = 'Ruby on Rails Tutorial Sample App'
 
-    return page_title.empty? ? base_title : (page_title << ' | ' << base_title)
+    return page_title.empty? ? base_title : (page_title + ' | ' + base_title)
   end
 end
--- rails_app/test/integration/users_show_test.rb
+++ rails_app/test/integration/users_show_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class UsersShowTest < ActionDispatch::IntegrationTest
   def setup
+    @user = users(:michael)
     @non_activated_user = users(:lana)
   end
 
@@ -13,5 +14,28 @@ class UsersShowTest < ActionDispatch::IntegrationTest
 
     # エラーメッセージ
     assert_not flash[:danger].empty?
+  end
+
+  test '正常系_ユーザー詳細' do
+    # ユーザー詳細を開く
+    get user_path(@user)
+
+    # タイトルなど要素のチェック
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+
+    # 投稿数がページに表示されているか
+    # 文字列に変換しないとマッチしない
+    assert_match @user.microposts.count.to_s, response.body
+
+    assert_select 'div.pagination'
+
+    # 投稿内容が正しく表示されているか
+    @user
+      .microposts
+      .paginate(page: 1)
+      .each { |micropost| assert_match micropost.content, response.body }
   end
 end
