3184979acb960439e3aa035ed267aa78eb5195d7
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-23 12:27:57 +0900

    micropostのシード

--- rails_app/db/seeds.rb
+++ rails_app/db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 最初のユーザーを作成
 User.create!(
   name: 'Example User',
   email: 'example@railstutorial.org',
@@ -15,6 +17,7 @@ User.create!(
   activated_at: Time.zone.now
 )
 
+# その他99人のユーザーを作成
 99.times do |time|
   name = Faker::Name.name
   email = "example-#{time + 1}@railstutorial.org"
@@ -27,4 +30,14 @@ User.create!(
     activated: true,
     activated_at: Time.zone.now
   )
+end
+
+# 最初に作られた6人のユーザーを取得
+users = User.order(:created_at).take(6)
+
+# 6人のユーザーごとに50投稿作成
+# タイムラインが1人のユーザーにならないように、ループ順を投稿→ユーザーにしている
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
