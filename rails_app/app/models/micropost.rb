5d0fdb4554e8cb06febdc06a7bc5c895b1cd097a
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-23 11:30:13 +0900

    micropostを新着順にソート

--- rails_app/app/models/micropost.rb
+++ rails_app/app/models/micropost.rb
@@ -1,6 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
+  # モデル取得時にデフォルトで新着順になるようにする
+  default_scope -> { order(created_at: :desc) }
+
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
 end
