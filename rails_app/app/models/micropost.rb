eda7c9d15c5594716512cf689fc60fc2b694378b
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-24 15:16:45 +0900

    画像のバリデーション(サイズ、拡張子)を追加

--- rails_app/app/models/micropost.rb
+++ rails_app/app/models/micropost.rb
@@ -9,4 +9,13 @@ class Micropost < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+  validate :picture_size
+
+  # アップロードされた画像のサイズをチェック
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, 'should be less than 5MB')
+    end
+  end
+  private :picture_size
 end
