ed1cc06f35b6faa4893338eadbd6151bf6c864f7
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-24 15:56:40 +0900

    本番でS3に画像を保存するための設定

--- rails_app/app/uploaders/picture_uploader.rb
+++ rails_app/app/uploaders/picture_uploader.rb
@@ -3,10 +3,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-
-  # storage :fog
+  # 本番ではfogを使い、開発/テストではapp/publicに保存する
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
